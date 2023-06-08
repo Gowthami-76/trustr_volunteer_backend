09097079e66a7f982fa0a3a0d6cd5bc0c5eb48a6
hanuramsandeep <hanuram.sandeep16>
2023-06-08 15:33:37 +0530

    gitignore

--- environment/development.js
+++ environment/development.js
@@ -2,7 +2,4 @@ module.exports = {
   PORT: "8443",
   dbUrl:
     "postgres://trustrdb:9unSM1TC7g9GGzZLIv@trustrdb.postgres.database.azure.com:5432/volunteerapp-dev",
-
-  // dbUrl:
-  //   "postgres://trustrdb:9unSM1TC7g9GGzZLIv@trustrdb.postgres.database.azure.com:5432/trustrdb-dev",
 };
